ebbdb746f5ee02b32b8e882fe6e50f2fe804f971
MinhNhat2710 <nhatdo2004@gmail.com>
2025-05-10 13:23:37 +0700

    lam bai tap 1

--- ChaoBan.java
+++
@@ -1,8 +0,0 @@
-// Tên file cần lưu là ChaoBan.java
-public class ChaoBan {
-
-    public static void main(String[] args) {
-        // In ra màn hình dòng chữ "Chào bạn!"
-        System.out.println("Chào bạn!");
-    }
-}
