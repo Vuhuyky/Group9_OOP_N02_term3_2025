181eefb2274a3fd138de8ee395abbb2a46679a7f
Vuhuyky <128498275+Vuhuyky@users.noreply.github.com>
2025-05-13 10:46:33 +0700

    BT_constructor

---
+++ ExcersiceClass/src/Sinhvien.java
@@ -0,0 +1,8 @@
+public class Sinhvien {
+    String maSinhvien;
+    String hoTen;
+    String ngaySinh;
+    Sinhvien(String maSinhvien, String hoTen, String ngaySinh){
+        this.maSinhvien = maSinhvien
+    }
+}
