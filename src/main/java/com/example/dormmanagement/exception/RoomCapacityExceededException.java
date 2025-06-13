1b61370a9b0d1de89ba8e7b6fbab7ebb79832abc
MinhNhat2710 <nhatdo2004@gmail.com>
2025-06-13 23:06:56 +0700

    lam lai

--- src/main/java/com/example/dormmanagement/exception/RoomCapacityExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.example.dormmanagement.exception;
-
-public class RoomCapacityExceededException extends RuntimeException {
-    public RoomCapacityExceededException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/dormmanagement/exception/RoomNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.dormmanagement.exception;
-
-public class RoomNotFoundException extends RuntimeException {
-    public RoomNotFoundException(String message) {
-        super(message);
-    }
-}
