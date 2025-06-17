458bc8fd51dfc7d20bd79b3e439799fb7bb6333f
Vuhuyky <128498275+Vuhuyky@users.noreply.github.com>
2025-06-17 11:32:11 +0700

    ok

--- DormitoryManagement/src/main/java/com/group9oop/dormitorymanagement/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.group9oop.dormitorymanagement.repository;
-
-import com.group9oop.dormitorymanagement.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, String> {
-    // Bạn có thể thêm các phương thức tùy chỉnh ở đây nếu cần
-}
