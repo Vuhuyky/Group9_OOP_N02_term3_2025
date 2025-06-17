ec3ddacfa8fc1b71bcaa12218c8218d1780a2f0c
Vuhuyky <128498275+Vuhuyky@users.noreply.github.com>
2025-06-17 12:09:08 +0700

    ...

---
+++ SpringBoot/complete/src/main/java/com/example/servingwebcontent/service/AdminService.java
@@ -0,0 +1,20 @@
+package com.example.servingwebcontent.service;
+
+import com.example.servingwebcontent.model.Admin;
+import com.example.servingwebcontent.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+    @Autowired
+    private AdminRepository adminRepository;
+
+    public Admin login(String username, String password) {
+        Admin admin = adminRepository.findByUsername(username);
+        if (admin != null && admin.getPassword().equals(password) && "ACTIVE".equalsIgnoreCase(admin.getStatus())) {
+            return admin;
+        }
+        return null;
+    }
+}
